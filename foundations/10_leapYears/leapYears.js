fed36879f6164eb185d9fe145a454069d1d86dc2
pinya <117656423+postscrippt@users.noreply.github.com>
2026-06-14 21:52:20 +0700

    Completed 10_leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    let leap = false
+    if (year > -1) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                leap = true
+            } else {
+                leap = false
+            }
+        } else {
+            if (year % 4 === 0) {
+                leap = true
+            }
+        }
+    }
+    return leap
 };
 
 // Do not edit below this line
