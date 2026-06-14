b831026fb1c901260fece5e768308ad818b7baf7
pinya <117656423+postscrippt@users.noreply.github.com>
2026-06-14 22:03:11 +0700

    Completed 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let celsius = (temp - 32) * (5 / 9)
+  return +celsius.toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let faren = ((temp * (9 / 5)) + 32)
+  return +faren.toFixed(1)
 };
 
 // Do not edit below this line
