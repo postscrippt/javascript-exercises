f124cddd80106bf733365d9fd38eba7fcdf25862
pinya <117656423+postscrippt@users.noreply.github.com>
2026-06-14 21:17:36 +0700

    completed 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...x) {
+    let removed = arr.filter(item => !x.includes(item)) //returns item if the unwanted is not included in the item
+
+    return removed
 };
 
 // Do not edit below this line
